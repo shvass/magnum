92828cdec81cbaf5f24e9ad2e2fe24929fc85940
shvass <akshayb@gmx.com>
2023-10-05 19:11:08 +0530

    initial commmit added translation

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "translate.hpp"
+
+
+int main(){
+    float x = 0.3, y = 0.7, speeds[4];
+
+    translate(x, y, speeds);
+
+    return 0;
+}
---
+++ translate.hpp
@@ -0,0 +1,40 @@
+#ifndef  TRANSLATE_HPP
+#define TRANSLATE_HPP
+
+#include <math.h>
+
+
+// max cords of joystick
+#define X_MAX 1.0
+#define Y_MAX 1.0
+
+// max speed of wheel 
+#define MAX_SPEED 1.0
+
+
+/**
+ * @brief 
+ *  calculate wheels speed, for translating the body
+ * based on joystick input cords
+ * 
+ * @param x joystick x cords
+ * @param y joystick y cords
+ * @param speeds array of speeds for wheels
+ *  speeds[0]  ->  A
+ *  speeds[1]  ->  B
+ *  speeds[2]  ->  C
+ *  speeds[3]  ->  D
+ */
+void translate(float x, float y, float* speeds){
+    // normalized coords
+    float normCords[] = { x / X_MAX, y / Y_MAX };
+
+    double theta = atan2(normCords[1], normCords[0]);
+    theta += M_PI / 4;
+
+    speeds[0] = speeds[2] = MAX_SPEED * sin(theta);
+    speeds[1] = speeds[4] = MAX_SPEED * cos(theta);
+}
+
+
+#endif //TRANSLATE_HPP
